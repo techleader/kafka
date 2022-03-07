54cb97cf3f42ec72a1346062b1c93fc9cdef2765
techleader <jlsnayak1983@gmail.com>
2022-03-08 03:08:23 +0530

    Added Kafka Producer

---
+++ src/main/java/com/learning/kafka/config/main/Application.java
@@ -0,0 +1,13 @@
+package com.learning.kafka.config.main;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+
+}
